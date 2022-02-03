f1af0d6e87c1a74554f4eb9520e6899ae8c177c6
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-02-03 14:08:12 +0100

    class and object added and calling done

--- fileread.py
+++ fileread.py
@@ -1,2 +1,13 @@
-class ReadFile:
+import secrets
 
+import pandas as pd
+
+
+class ReadFile:
+    def __init__(self,filedata):
+        self.filedata = filedata
+
+    def excel_file(self):
+        colcountry = self.filedata['Country'].values
+        randomvalue = secrets.choice(colcountry)
+        return randomvalue
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import random
 import pandas as pd
+from fileread import ReadFile
 
 
 def take_input():
     x = 0
     try:
-        full_word = input("Enter a word: ")
+        exceldata = pd.read_excel(r'Words.xlsx')
+        wordObj = ReadFile(exceldata)
+        full_word = wordObj.excel_file()
+        # full_word = input("Enter a word: ")
     except ValueError:
-        print("Error in word.")
+        print("Error in File.")
     length_true = len(full_word)
     lengths = int(length_true / 2)
+    guessnumber = str(lengths)
     full_list = list(full_word)
     while x < lengths:
         full_list = random.sample(full_list, length_true)
         full_list = ''.join(full_list)
         x += 1
-    print("Word is: " + full_list)
+    print("Word is: " + full_list + " and you have " + guessnumber + " guesses.")
     return [lengths, full_word]
 
 
@@ -26,7 +31,7 @@ def check_words():
     limit_off = False
     while list_data[1] != guess_word and not limit_off:
         if counter < list_data[0]:
-            guess_word = input("Enter a guess word: ")
+            guess_word = input(f"This is chance " + str(counter+1) + ". Enter a guess word: ")
             counter += 1
         else:
             limit_off = True
