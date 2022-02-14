96e20b554154de8e3f130cf0dee335b6f56d7305
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-02-14 20:49:51 +0100

    -added answer check -score count -score display

--- Quiz.py
+++ Quiz.py
@@ -2,19 +2,31 @@ import random
 import pandas as pd
 from fileread import ReadFile
 
+userscore = 0
+totalquestion = 0
+
 
 def takeMainInput():
-    exceldata = pd.read_excel(r'Words.xlsx')
-    newfileobj = ReadFile(exceldata)
-    quizansCapital = newfileobj.excel_file2()
-    quizansCountry =newfileobj.excel_file()
+    global userscore
+    global totalquestion
+    exceldata = pd.read_excel(r'Words.xlsx')  # read file
+    newfileobj = ReadFile(exceldata)  # create onj
+    quizansCapital = newfileobj.excel_file2()  # read capital column
+    quizansCountry = newfileobj.excel_file()  # read country column
     lencountry = len(quizansCountry)
-    print(quizansCountry, quizansCapital)
     for x in range(lencountry):
-        print("What is the capital of " + str(quizansCountry[x]))
+        totalquestion += 1  # count num of question
+        quizansCapital[x] = quizansCapital[x].upper()
+        useransCapital = str(
+            input("What is the capital of " + str(quizansCountry[x]).upper() + ": ")).upper()  # user ans
+        if useransCapital == str(quizansCapital[x]):
+            userscore += 1
+            print("Proceeding to next question....\n")
+        else:
+            userscore += 0
+            print("Proceeding to next question....\n")
 
-
-
+    print("Your score is " + str(userscore) + "\\" + str(totalquestion) + ".")
 
 
 takeMainInput()
