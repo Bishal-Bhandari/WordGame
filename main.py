08027abd1c83a2c79ed7e276307d07dd1102f19a
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-02-16 21:14:23 +0100

    -looping bug in the main after playing game solved -looping problem after pressing no in guessing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from importlib import reload
+
 print("\n===>>WELCOME to our game word and quiz.<<===")
 
 
@@ -8,9 +10,17 @@ def gamechoice():
             choice = input("\nPlease press 'a' or 1 for Word Guess Game and press 'b' or 2 for Quiz. Also, 'c' or 3 to "
                            "exit: ")
             if choice == "a" or choice == "A" or choice == str(1):
-                import Guessword  # importing guess word file
+                # importing guess word file
+                if 'Guessword' in dir():  # if it has already been imported
+                    reload(Guessword)
+                else:
+                    import Guessword
             elif choice == "b" or choice == "B" or choice == str(2):
-                import Quiz  # importing quiz file
+                # importing quiz file
+                if 'Quiz' in dir():  # if it has already been imported
+                    reload(Quiz)
+                else:
+                    import Quiz
             elif choice == "c" or choice == "C" or choice == str(3):
                 print("Thank you for playing.")
                 break
